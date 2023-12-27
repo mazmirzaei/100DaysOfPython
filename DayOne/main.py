992327b8949b16148f3f824348925a47c724a435
Maz <>
2023-12-26 19:14:03 -0800

    Brand generator

--- DayOne/main.py
+++ DayOne/main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Band name generator
+print("Welcome to the band name generator!")
+city_name = input("What is your city you grow up in? \n")
+pets_name = input("What is your pets name? \n")
+print("Your brand name could be : " + city_name + " " + pets_name)
